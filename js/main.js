8159b5c607a7011d1c972feb92b7e91a5fc21f5f
nelsonbuhler <122967704+nelsonbuhler@users.noreply.github.com>
2023-12-04 16:49:31 +0100

    finition du logo du burger menu

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 var burger = document.querySelector(".burger");
 var menu = document.querySelector(".menu");
-var titlebox = document.querySelector(".titleburger");
+var titleburger = document.querySelector(".titleburger");
 
 var toggleMenu = function toggleMenu() {
   menu.classList.toggle("is-active");
   burger.classList.toggle("is-active");
-  titlebox.classList.toggle("is-active");
+  titleburger.classList.toggle("is-active");
 };
 
 burger.addEventListener("click", toggleMenu);
